59ea3c806f29641c54543a2b2ff239baab397929
Nexr0n <39227048+Nexr0n@users.noreply.github.com>
2021-02-06 20:53:02 -0500

    Improve documentation

--- York SSO Autologin.user.js
+++ York SSO Autologin.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://passportyork.yorku.ca/ppylogin/ppylogin
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Judah Goldstein
 // @description clicks the login button automaticaly when the SSO page loads, only works if you have autofill on.
 // ==/UserScript==
@@ -12,9 +12,9 @@ window.onload = function() {
   var button = document.getElementsByName("dologin"); //finds login button
   var username = document.getElementById("mli");      //finds username box
   var password = document.getElementById("password"); //finds password box
-  if(username.value != "" && password.value != ""){
-    console.log("working!");
-    button[0].click();
+  if(username.value != "" && password.value != ""){ //makes sure you've actually got the username and password filled in
+    console.log("success, logging you in");
+    button[0].click(); //hits login button 
   } else {
     alert("I'm happy that you have such confidence in my mind reading abilities but this script only works with autofill enabled for your YorkU account's username and password through your browser. if this is a mistake please make an issue on my github at https://github.com/Nexr0n/York-SSO-Autologin");
   }
